dbe49c55553a3512aa695523d8e2ce9a65f34921
bob5390 <jothma02@gmail.com>
2026-02-25 23:27:07 -0700

    Added basic endpoint layout

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,26 @@
 package server;
 
+import exception.ResponseException;
 import io.javalin.*;
 
 public class Server {
+    private final ChessService service;
 
     private final Javalin javalin;
 
     public Server() {
+        service = new ChessService();
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
-
+        javalin.before(null)
+               .get("/game", this::listGames)
+               .post("/game", this::createGame)
+               .post("/user", this::registerUser)
+               .post("/session", this::login)
+               .put("/game", this::joinGame)
+               .delete("/session", this::logout)
+               .delete("/db", this::clearDatabases);
     }
 
     public int run(int desiredPort) {
@@ -20,5 +30,51 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    private void listGames(Context ctx) throws ResponseException {
+        ctx.result(service.listGames().toString());
+    }
+
+    private void createGame(Context ctx) throws ResponseException {
+        // make game from json
+        // make create game request and send off to handler
+        // result to json
+        ctx.result();
+    }
+
+    private void joinGame(Context ctx) throws ResponseException {
+        // make join game request
+        // send off to handler
+        // result to json
+        ctx.result();
+    }
+
+    private void clearDatabases(Context ctx) throws ResponseException {
+        // clear request
+        // send off to handler
+        // result to json
+        ctx.result();
+    }
+
+    private void registerUser(Context ctx) throws ResponseException {
+        // make register request
+        // send off to handler
+        // result to json
+        ctx.result();
+    }
+
+    private void login(Context ctx) throws ResponseException {
+        // login request
+        // send to handler
+        // result to json
+        ctx.result();
+    }
+
+    private void logout(Context ctx) throws ResponseException {
+        // logout request
+        // send to handler
+        // result to json
+        ctx.result();
     }
 }
